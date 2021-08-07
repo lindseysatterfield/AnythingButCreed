f717b728f74dbabd697385ab25a843fa7b708ae8
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-08-07 10:00:37 -0500

    Add project files.

---
+++ AnythingButCreed/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnythingButCreed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var goodSongs = new List<Song>();
+            var allSongs = new List<Song>();
+
+            allSongs.Add(new Song("Creed", "With Arms Wide Open"));
+            allSongs.Add(new Song("Creed", "Higher"));
+            allSongs.Add(new Song("Creed", "My Sacrifice"));
+            allSongs.Add(new Song("Backstreet Boys", "Larger than Life"));
+            allSongs.Add(new Song("Backstreet Boys", "I Want It That Way"));
+            allSongs.Add(new Song("Backstreet Boys", "Quit Playing Games"));
+            allSongs.Add(new Song("NSYNC", "Bye Bye Bye"));
+            allSongs.Add(new Song("NSYNC", "It's Gonna Be Me"));
+
+            goodSongs = allSongs.Where(x => x.Artist != "Creed").ToList();
+
+            foreach (var song in goodSongs)
+            {
+                Console.WriteLine($"{song.Artist} : {song.Name}");
+            }
+        }
+    }
+}
